ff3f17920186636e9a81f04921e981a7132e0530
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-06-02 22:31:01 +0300

    start movies page

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,20 +1,20 @@
 import { Container } from 'components/Container';
-import { Loader } from 'components/Loader';
-import { MoviesList } from 'components/MoviesList';
-import { MoviesListItem } from 'components/MoviesList/MoviesListItem';
+// import { Loader } from 'components/Loader';
+// import { MoviesList } from 'components/MoviesList';
+// import { MoviesListItem } from 'components/MoviesList/MoviesListItem';
 import { SearchMovieForm } from 'components/SearchMovieForm';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 export const MoviesPage = () => {
   let [searchParams, setSearchParams] = useSearchParams();
 
-  const query = searchParams.get('query');
+  // const query = searchParams.get('query');
 
   const handleFormSubmit = searchQuery => {
     setSearchParams({ query: searchQuery });
   };
-
+  console.log(searchParams);
   // if (loading) return <Loader />;
 
   return (
